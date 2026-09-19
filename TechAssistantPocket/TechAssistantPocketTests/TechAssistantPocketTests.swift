4d1ab4130502eb2b65e4f57c696692bac8ed0d3e
腐った卵 <satoukento@satoukentounoMacBook-Pro-2.local>
2026-09-19 22:46:31 +0900

    Bootstrap iOS app project

---
+++ TechAssistantPocket/TechAssistantPocketTests/TechAssistantPocketTests.swift
@@ -0,0 +1,17 @@
+//
+//  TechAssistantPocketTests.swift
+//  TechAssistantPocketTests
+//
+//  Created by 腐った卵 on 2026/09/19.
+//
+
+import Testing
+@testable import TechAssistantPocket
+
+struct TechAssistantPocketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
